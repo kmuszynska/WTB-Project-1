58c71cc4bbbd4d2e5b8547013694bb9cdc1034af
Kuba <jakub.golec90@gmail.com>
2019-06-29 13:07:31 +0200

    Initial commit

---
+++ src/main/java/com/WTBProject/WTBProject/WtbProjectApplication.java
@@ -0,0 +1,13 @@
+package com.WTBProject.WTBProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WtbProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WtbProjectApplication.class, args);
+	}
+
+}
